1fbac5289e80f164c67318d25020cd0fae426c14
Brady McDermott <bradymcdermott202@yahoo.com>
2021-11-20 21:24:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function moreinfo(beer) {
     beerog.className = "show";
     beeribu.className = "show";
     close.className = "show";
-    popupimage.src = "Images/"+beer+".png";
+    popupimage.src = "./Images/"+beer+".png";
 }
 
 function popupclose() {
@@ -50,9 +50,9 @@ function popupclose() {
 }
 
 function canhover(element, beer) {
-    element.src = "Images/"+beer+".gif";
+    element.src = "./Images/"+beer+".gif";
 }
 
 function cannohover(element, beer) {
-    element.src = "Images/"+beer+".png";
+    element.src = "./Images/"+beer+".png";
 }
